cc403105b015afbe16d0da1d6dd0eaae4029176f
bthomas218 <bradythomas690@gmail.com>
2025-10-01 07:46:13 -0500

    feat: added list_characters()

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, list_characters
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -10,5 +10,7 @@ def main():
     num_words = count_words(text)
     char_counter = count_characters(text)
     print(f"Found {num_words} total words")
-    print(f"{char_counter}")
+    for item in list_characters(char_counter):
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,13 @@ def count_characters(text):
         else:
             chars[char.lower()] = 1
     return chars
+
+def sort_on(items):
+    return items["num"]
+
+def list_characters(dictionary):
+    new_list = []
+    for key, value in dictionary.items():
+        new_list.append({"char": key, "num": value})
+    new_list.sort(reverse=True, key=sort_on)
+    return new_list
