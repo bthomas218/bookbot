61c02f4e70e0281588bc6bcc6da09ae505247341
bthomas218 <bradythomas690@gmail.com>
2025-10-01 07:56:30 -0500

    feat: added arguments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words, count_characters, list_characters
 
 def get_book_text(file_path):
@@ -6,9 +7,14 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    text = get_book_text(sys.argv[1])
+
     num_words = count_words(text)
     char_counter = count_characters(text)
+
     print(f"Found {num_words} total words")
     for item in list_characters(char_counter):
         if item["char"].isalpha():
